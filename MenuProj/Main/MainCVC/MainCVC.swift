70bca86c345000684c1e48a4dcc05c8fe8d4cb46
IvanyukovI <98633968+IvanyukovI@users.noreply.github.com>
2022-12-27 19:26:32 +0700

    Start

---
+++ MenuProj/Main/MainCVC/MainCVC.swift
@@ -0,0 +1,17 @@
+//
+//  MainCVC.swift
+//  MenuProj
+//
+//  Created by Игорь Иванюков on 26.12.2022.
+//
+
+import UIKit
+
+class MainCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
